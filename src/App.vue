200ea874d620c0cfc83e9886102787f66fb269ce
BaxromAvliyoqulov <avliyoqulovbaxrom@gmail.com>
2025-03-20 18:22:23 +0500

    google icon

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
 						style="background-color: white; border: 1px solid blue; color: blue"
 					>
 						<img
-							src="@/assets/google-icon.png"
+							src="img/google.png"
 							alt="Google Icon"
 							style="width: 16px; height: 16px; margin-right: 8px"
 						/>
