c9d459daf05165b8aa5843e9da073484ca2c2f52
BaxromAvliyoqulov <avliyoqulovbaxrom@gmail.com>
2025-03-20 18:15:24 +0500

    errors

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
 						style="background-color: white; border: 1px solid blue; color: blue"
 					>
 						<img
-							src="path/to/google-icon.png"
+							src="@/assets/google-icon.png"
 							alt="Google Icon"
 							style="width: 16px; height: 16px; margin-right: 8px"
 						/>
